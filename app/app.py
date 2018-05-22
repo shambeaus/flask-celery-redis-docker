a1b3b12c748362d4af59ec9615d214ef3d0e1cca
step4823 <stephen.shambeau@rackspace.com>
2018-05-21 20:06:53 -0500

    new

--- app/app.py
+++ app/app.py
@@ -1,48 +1,7 @@
-from flask import Flask, abort, request, jsonify, Response, url_for, session
-from task import long_task
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/api/receivedata', methods=['POST'])
-def receive_data():
-    x = request.json
-    return jsonify(x)
-
-
-@app.route('/api/longtask', methods=['POST'])
-def longtask():
-    task = long_task.apply_async()
-    return jsonify({}), 202, {'Location': url_for('taskstatus',
-                                                  task_id=task.id)}
-
-@app.route('/api/status/<task_id>')
-def taskstatus(task_id):
-    task = long_task.AsyncResult(task_id)
-    if task.state == 'PENDING':
-        response = {
-            'state': task.state,
-            'current': 0,
-            'total': 1,
-            'status': 'Pending...'
-        }
-    elif task.state != 'FAILURE':
-        response = {
-            'state': task.state,
-            'current': task.info.get('current', 0),
-            'total': task.info.get('total', 1),
-            'status': task.info.get('status', '')
-        }
-        if 'result' in task.info:
-            response['result'] = task.info['result']
-    else:
-        # something went wrong in the background job
-        response = {
-            'state': task.state,
-            'current': 1,
-            'total': 1,
-            'status': str(task.info),  # this is the exception raised
-        }
-    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(debug=True)
